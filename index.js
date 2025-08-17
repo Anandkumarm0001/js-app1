3c506fada0d5839a881f3dce351b0d471af61fa8
Anand Mahato <167707816+Anandkumarm0001@users.noreply.github.com>
2025-08-17 09:11:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function getcolor () {
+const randomnumber = Math.floor(Math.random() * 16777215);
+const randomcode = "#" + randomnumber.toString(16);
+console.log(randomnumber , randomcode);
+document.body.style.background = randomcode;
+document.getElementById("color-code").innerText = randomcode;
+navigator.clipboard.writeText(randomcode)
+} 
+
+document.getElementById("btn").addEventListener(
+  "click", getcolor
+)
+getcolor()
